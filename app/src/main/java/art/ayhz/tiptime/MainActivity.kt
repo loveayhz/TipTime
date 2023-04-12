d4aaf10d27480f07f60cf92ea3f11a091f56a302
“loveayhz” <3446632075@qq.com>
2023-04-12 17:21:16 +0800

    feat: 在文本框输入完成后添加事件，在amount输入完成(键盘回车或对号)后focus到下一个文本框，tip输入完成后清除focus并收起键盘输入

--- app/src/main/java/art/ayhz/tiptime/MainActivity.kt
+++ app/src/main/java/art/ayhz/tiptime/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,8 +14,11 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,13 +59,15 @@ fun DefaultPreview() {
 }
 
 @Composable
-fun TipTimeScreen(){
+fun TipTimeScreen() {
     var amountInput by remember { mutableStateOf("") }
     var tipPercentInput by remember { mutableStateOf("") }
-    val amount = amountInput.toDoubleOrNull()?: 0.0
-    val tipPercent = tipPercentInput.toDoubleOrNull()?: 0.0
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tipPercent = tipPercentInput.toDoubleOrNull() ?: 0.0
     //val tip = calculateTip(amount)
-    val tip = calculateTip(amount,tipPercent)
+    val tip = calculateTip(amount, tipPercent)
+
+    val focusManager = LocalFocusManager.current
     Column(modifier = Modifier.padding(32.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
         Text(
             text = stringResource(id = R.string.calculate_tip),
@@ -70,45 +76,62 @@ fun TipTimeScreen(){
         )
         Spacer(modifier = Modifier.height(16.dp))
         EditNumberField(
-            value = amountInput,
             label = R.string.bill_amount,
-            onValueChange = {amountInput = it}
+            value = amountInput,
+            onValueChange = { amountInput = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) })
         )
-
         EditNumberField(
-            value = tipPercentInput,
             label = R.string.tip_percent,
-            onValueChange = {tipPercentInput = it}
+            value = tipPercentInput,
+            onValueChange = { tipPercentInput = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
         )
         Spacer(modifier = Modifier.height(16.dp))
         Text(
-            text = stringResource(id = R.string.tip_amount,tip),
+            text = stringResource(id = R.string.tip_amount, tip),
             modifier = Modifier.align(Alignment.CenterHorizontally),
             fontSize = 26.sp,
             fontWeight = FontWeight.Bold
         )
-        
+
     }
 }
 
 
 @Composable
-fun EditNumberField(value: String,@StringRes label: Int, onValueChange: (String)-> Unit,modifier: Modifier = Modifier) {
-
+fun EditNumberField(
+    @StringRes label: Int,
+    value: String,
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions,
+    keyboardActions: KeyboardActions,
+    modifier: Modifier = Modifier
+) {
     TextField(
         value = value,
-//        onValueChange = { amountInput = it},
         onValueChange = onValueChange,
-        label = { Text(
-            text = stringResource(id = label),
-            modifier = Modifier
-        )},
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        label = {
+            Text(
+                text = stringResource(id = label),
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
         singleLine = true
     )
 }
 
-private fun calculateTip(amount: Double,tipPercent: Double = 15.0):String{
-    var tip = amount * tipPercent/100
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+    var tip = amount * tipPercent / 100
     return NumberFormat.getCurrencyInstance().format(tip)
 }
